dc9acd56a07c3f64ecceeb6e9acfdc0ea7d9960b
Cesar <guto_montaldi@yahoo.com.br>
2023-01-17 15:10:13 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dep = new Department(2, "Notebooks");
+		
+		System.out.println(dep);
+
+	}
+
+}
